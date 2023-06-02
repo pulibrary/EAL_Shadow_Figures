b515af1636fbf51d577e5f9e01f7fd590bf41588
cwulfman <cwulfman@princeton.edu>
2023-06-02 16:45:26 -0400

    first pass object pages done

--- main.py
+++ main.py
@@ -38,44 +38,19 @@ for record in records:
 
     object_index.append(metadata)
 
-    # if record.objecttype not in type_index:
-    #     type_index[record.objecttype] = []
-
-    # type_index[record.objecttype].append(record.objectno)
-
 environment = Environment(loader=FileSystemLoader("templates"))
 
-
-test_objects = [
-    {
-        'objectno': '1',
-        'objecttype': 'Dan head',
-        'images': ['DSCN4746.JPG'],
-        'description': 'Dan head. Wu. Dan. Woman warrior. Black, open cut face, small earring. Large red central pompom; blue tone diadem, ezi 額子, with red pompom at ear; red tassel hangs in front of neck. Black hair bound in figure eight by twin meifa 美髮 sticks. Filigree cut ornament hangs below.',
-        'dimensions': 'H:',
-    },
-    {
-        'objectno': '2',
-        'objecttype': 'Dan head',
-        'images': ['DSCN4746.JPG'],
-        'description': 'Dan head. Wu. Dan. Woman warrior. Black, open cut face, small earring. Large red central pompom; blue tone diadem, ezi 額子, with red pompom at ear; red tassel hangs in front of neck. Black hair bound in figure eight by twin meifa 美髮 sticks. Filigree cut ornament hangs below.',
-        'dimensions': 'H:',
-    },
-]
 
 template = environment.get_template("objects.html")
 
 base_path: Path = Path("site")
+paths = []
+for obj_type in types:
+    page: str = f"{obj_type.replace(' ', '_')}.html"
+    paths.append({"label": obj_type, "link": page})
+
 for obj_type in types:
     path: Path = base_path / Path(obj_type.replace(" ", "_"))
     context = [obj for obj in object_index if obj['objecttype'] == obj_type]
     with open(path.with_suffix(".html"), mode="w", encoding="utf-8") as f:
-        f.write(template.render(objects=context, type=obj_type))
-
-# context = {"objects": object_index}
-
-# content = template.render(context)
-
-
-# with open("/tmp/thunk.html", mode="w", encoding="utf-8") as f:
-#     f.write(content)
+        f.write(template.render(objects=context, nav=paths, type=obj_type))
