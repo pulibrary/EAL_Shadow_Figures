688eebd09482f5740c5310e76714986d2d14952e
cwulfman <cwulfman@princeton.edu>
2023-06-04 12:01:28 -0400

    ready to generate image pages

--- shadows.py
+++ shadows.py
@@ -14,6 +14,7 @@ class ShadowImage:
     def __init__(self, filename: str) -> None:
         self.filename = filename
         self.id = filename.split('.')[0]
+        self.associated_objects = []
 
     def __repr__(self) -> str:
         return f"ShadowImage({self.id})"
@@ -93,7 +94,7 @@ class TypePage(Page):
 
 class ImagePage(Page):
     def __init__(self, shadow_image: ShadowImage) -> None:
-        super().__init__(self, shadow_image.id)
+        super().__init__(self)
         self.shadow_image: ShadowImage = shadow_image
 
 
@@ -103,20 +104,46 @@ def all_types(obj_list: list[ShadowObject]) -> set:
 
 class ShadowFigureSiteGenerator:
     def __init__(self, source_path: Path) -> None:
-        self.object_records = None
+        self.shadow_objects = None
+        self.shadow_images = None
+        self._image_index: Optional[dict] = None
         self._type_pages: Optional[list[TypePage]] = None
+        self._image_pages: Optional[list[ImagePage]] = None
         self.read(source_path)
 
     def read(self, file_path: Path) -> None:
         with open(file_path, mode='r', encoding='utf-8') as f:
             reader = DictReader(f)
             records = [ObjectRecord(**row) for row in reader]
-        self.object_records = [ShadowObject(record) for record in records]
+        self.shadow_objects = [ShadowObject(record) for record in records]
+
+    @property
+    def image_index(self) -> dict:
+        if self._image_index is None:
+            self._image_index = {}
+            for object in self.shadow_objects:
+                for shadow_image in object.associated_images:
+                    if shadow_image.id not in self._image_index:
+                        indexed_image = ShadowImage(shadow_image.filename)
+                        self._image_index[shadow_image.id] = indexed_image
+                    else:
+                        indexed_image = self._image_index[shadow_image.id]
+
+                    indexed_image.associated_objects.append(object)
+        return self._image_index
 
     @property
     def type_pages(self) -> list[TypePage]:
         if self._type_pages is None:
             self._type_pages = []
-            for type in all_types(self.object_records):
-                self.type_pages.append(TypePage(type, self.object_records))
+            for type in all_types(self.shadow_objects):
+                self.type_pages.append(TypePage(type, self.shadow_objects))
         return self._type_pages
+
+    @property
+    def image_pages(self) -> list[ImagePage]:
+        if self._image_pages is None:
+            self._image_pages = []
+            for index, shadow_image in self.image_index.items():
+                self._image_pages.append(ImagePage(shadow_image))
+        return self._image_pages
